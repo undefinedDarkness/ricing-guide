b8fb514488495a76108aa474a6d23d1a40c0f7e2
undefinedDarkness <davidnoronha@outlook.in>
2024-05-08 15:20:32 +0530

    mv embed to correct location

---
+++ .tooling/sync_with_personal.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp -rv ../undefinedDarkness.github.io/.tooling ./.tooling
+cp -rv ../undefinedDarkness.github.io/generate ./
+cp -rv ../undefinedDarkness.github.io/assets/*.css ./assets/ 
